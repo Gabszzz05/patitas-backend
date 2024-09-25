ea66ec8d772642b2fbe6335a16ca10a04458bb39
Gabszzz05 <gabszzz005@gmail.com>
2024-09-24 22:14:24 -0500

    Implementacion de Duration de respuesta al FrontEnd

--- src/main/java/pe/edu/cibertec/patitas_backend/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend/controller/AutenticacionController.java
@@ -10,6 +10,9 @@ import pe.edu.cibertec.patitas_backend.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas_backend.service.AutenticacionService;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.time.Duration;
+import java.util.Arrays;
 
 @RestController//Permitira hacer e llamado como un servicio
 @RequestMapping("/autenticacion")
@@ -22,12 +25,18 @@ public class AutenticacionController {
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO){
 
         try {
+            //Agregamos esto para comprobar el tiempo que se le asigno al front para que tenga una respuesta
+            //Thread.sleep(Duration.ofSeconds(10));
             String[] datosUsuario = autenticacionService.validarUsuario(loginRequestDTO);
+            //Imprimimos el array(No necesario)
+            System.out.println("Resultado: " + Arrays.toString(datosUsuario));
             //
             if(datosUsuario == null){
                 return new LoginResponseDTO("01", "ERROR: Usuario no encontrado", "", "");
             }
             return new LoginResponseDTO("00", "", datosUsuario[0], datosUsuario[1]);
+            //Agregamos el InterruptedException
+            // | InterruptedException
         } catch (IOException e) {
             return new LoginResponseDTO("99", "ERROR: Ocurrio un problema", "", "");
         }
